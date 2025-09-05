afe6bfde052a2a0eb015db76dccc10d575c82433
Suvam Naskar <suvamnaskar780@gmail.com>
2025-09-05 10:07:16 +0530

    Refactor server.js to establish MongoDB connection and set up view engine

--- server.js
+++ server.js
@@ -1,16 +1,26 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const connectDB = require('./mongo_connect');
 const todoRoutes = require('./routes/Todos');
+const homeRoute = require('./routes/home');
 
 
 const app = express();
 
+// Connect to MongoDB
+connectDB();
+
 // Middleware
 app.use(cors());
 app.use(express.json()); // Allows us to parse JSON in the request body
+app.use(express.urlencoded({ extended: true })); // Allows us to parse URL-encoded form data
 app.use(express.static('public'));
 
+// Set view engine
+app.set('view engine', 'ejs');
+
+app.use('/', homeRoute);
 app.use('/todos', todoRoutes);
 
 // --- Start the Server ---
