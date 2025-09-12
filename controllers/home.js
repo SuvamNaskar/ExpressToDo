6a918c2adcfa7073f99bc2ee6cecdae9641190cd
Suvam Naskar <suvamnaskar780@gmail.com>
2025-09-12 14:15:38 +0530

    Add handleHome and handleLogin functions to manage home and login requests

--- controllers/home.js
+++ controllers/home.js
@@ -1,5 +1,43 @@
 const User = require('../models/Users');
 const bcrypt = require('bcrypt');
+const { getTodos } = require('./apis');
+
+const handleHome = async (req, res) => {
+    try {
+        const todos = await getTodos();
+        res.render('index', { todos: todos });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+}
+
+const handleLogin = async (req, res) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return res.status(400).json({ message: 'All fields are required' });
+    }
+
+    try {
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(401).json({ message: 'Invalid email or password' });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.passwordHash);
+        if (!isMatch) {
+            return res.status(401).json({ message: 'Invalid email or password' });
+        }
+
+        res.status(200).json({ message: 'Login successful' });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+}
+
+const handleLoginUi = (req, res) => {
+    res.render('login');
+}
 
 const handleSignupUi = (req, res) => {
     res.render('signup');
@@ -33,4 +71,4 @@ const handleSignup = async (req, res) => {
     }
 }
 
-module.exports = { handleSignupUi, handleSignup };
+module.exports = { handleSignupUi, handleSignup, handleLoginUi, handleLogin, handleHome };
